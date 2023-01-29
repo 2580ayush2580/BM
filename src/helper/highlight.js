d680cd18b3619341fe1ccb6bede696a7bc39a694
Ayush Kumar <2580ayush2580@gmail.com>
2023-01-29 16:16:00 +0530

    highlighted-data logic fix

--- src/helper/highlight.js
+++ src/helper/highlight.js
@@ -3,23 +3,14 @@ export const getHighlightedData = (arrGiven, budgetGiven) => {
   const arr = temp_arr.sort((a, b) => a.amount - b.amount);
   const budget = Number(budgetGiven || 0);
   const newArray = [];
-  let i = 0;
   let j = arr.length - 1;
   let ans = 0;
-  while (i <= j) {
+  while (0 <= j) {
     if (ans + Number(arr[j].amount) > budget) j--;
     else {
       newArray.push(arr[j].id);
       ans += Number(arr[j--].amount);
     }
-    if (i < j) {
-      if (i !== j && ans + Number(arr[i].amount) > budget) i++;
-      else {
-        newArray.push(arr[i].id);
-        ans += Number(arr[i++].amount);
-      }
-    }
   }
-
   return newArray;
 };
